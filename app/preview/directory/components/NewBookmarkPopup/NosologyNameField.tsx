5027402c4b588f859cf5b04fcedbe93c7d1c6585
Lily <mrs.esmirn@gmail.com>
2026-06-03 13:23:01 +0500

    fix: text on popup

--- app/preview/directory/components/NewBookmarkPopup/NosologyNameField.tsx
+++ app/preview/directory/components/NewBookmarkPopup/NosologyNameField.tsx
@@ -14,13 +14,12 @@ export default function NosologyNameField({
 }: NosologyNameFieldProps) {
   return (
     <label className={styles.nameLabel}>
-      <span className={styles.nameLabelText}>Название нозологии</span>
       <input
         ref={inputRef}
         value={value}
         onChange={(event) => onChange(event.target.value)}
         className={styles.nameInput}
-        placeholder="Введите название нозологии"
+        placeholder="Введите название закладки"
       />
     </label>
   );
